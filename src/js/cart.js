8c3c57e40833198068eff54cadcd633e568a6c54
andrii-pozniak <andriipozniak@gmail.com>
2022-10-07 15:50:52 +0300

    1

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,7 +1,7 @@
-export default function onCart({backdrop_path, name, title, vote_average}) {
-    const BASE_IMAGE = `https://image.tmdb.org/t/p/w500`
+export default function onCart({poster_path, name, title, vote_average}) {
+    const BASE_IMAGE = `https://image.tmdb.org/t/p/original`
     return `<div class="cart">
-    <img src="${BASE_IMAGE}${backdrop_path}" alt="A FISTFUL OF LEAD" class="cart_image" width="300">
+    <img src="${BASE_IMAGE}${poster_path}" alt="A FISTFUL OF LEAD" class="cart_image" width="300">
     <h2 class="cart_name">${name||title}</h2>
     <p class="cart_title">Genre <span class="cart_span">Year </span><span class="cart_span-rating">  ${vote_average}</span></p>
 </div>`
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -6,10 +6,9 @@ const apiService = new ApiService ();
 apiService.fetchImage().then(data => {
     addArticleImage(data);
 })
-console.log(document.querySelector(`body`));
 
 function addArticleImage(data) { 
- 
+    console.log(data.results);
     const cart = data.results.map(result => onCart(result)).join("");
     document.querySelector(`body`).insertAdjacentHTML("beforeend", cart)
      
