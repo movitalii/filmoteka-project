12fd20db5c06da8fbf39e61403691b7ffead4d7b
anfernee84 <eduard.rogulin@outlook.com>
2022-10-08 14:53:31 +0300

    Update api_service.js

--- src/js/api_service.js
+++ src/js/api_service.js
@@ -4,34 +4,52 @@ export const KEY = '119d79b053f922516c6af4b71b0fd3ac';
 
 axios.defaults.baseURL = `https://api.themoviedb.org/3`;
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const getPopularMovies = async (page = 1) => {
   const { data } = await axios.get(
     `/trending/movie/week?api_key=${KEY}&page=${page}`
   );
-  console.log(data);
+  console.log(data.results);
   return data;
 };
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const movieSearchByName = async (query, page) => {
-  //   try {
-  //     return await axios
-  //       .get(
-  //         `/search/movie?api_key=${KEY}&language=en-US&query=${query}&page=${page}`
-  //       )
-  //       .then(res => {
-  //         console.log(res);
-  //         return res;
-  //       });
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
+  try {
+    const objResult = [];
+    return await axios
+      .get(
+        `/search/movie?api_key=${KEY}&language=en-US&query=${query}&page=${page}`
+      )
+      .then(res => {
+        res.data.results.forEach(element => {
+          objResult.push(element);
+        });
+        console.log(
+          objResult.reduce(function (newArr, movie) {
+            if (movie.backdrop_path != null) {
+              newArr.push(movie);
+            }
+            return newArr;
+          }, [])
+        );
+        return objResult.reduce(function (newArr, movie) {
+          if (movie.backdrop_path != null) {
+            newArr.push(movie);
+          }
+          return newArr;
+        }, []);
+      });
+  } catch (error) {
+    console.error(error);
+  }
 
-  const { data } = await axios.get(
-    `/search/movie?api_key=${KEY}&language=en-US&query=${query}&page=${page}`
-  );
-  console.log(data);
-  return data;
+  // const { data } = await axios.get(
+  //   `/search/movie?api_key=${KEY}&language=en-US&query=${query}&page=${page}`
+  // );
+  // console.log(data);
+  // return data;
 };
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const getMovieDetails = async id => {
   const { data } = await axios.get(`/movie/${id}?api_key=${KEY}`);
