8a31fad7bfe5d771882a8bf6f378dfc3d362cc9f
anfernee84 <eduard.rogulin@outlook.com>
2022-10-08 15:45:44 +0300

    Update api_service.js

--- src/js/api_service.js
+++ src/js/api_service.js
@@ -1,62 +1,34 @@
 import axios from 'axios';
 
-export const KEY = '119d79b053f922516c6af4b71b0fd3ac';
+export const KEY = '3ff086ca8fded08ba42938358b3327b4';
 
 axios.defaults.baseURL = `https://api.themoviedb.org/3`;
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////// get popular https://developers.themoviedb.org/3/movies/get-popular-movies
 const getPopularMovies = async (page = 1) => {
   const { data } = await axios.get(
-    `/trending/movie/week?api_key=${KEY}&page=${page}`
+    `/movie/popular?api_key=${KEY}&page=${page}`
   );
   console.log(data.results);
   return data;
 };
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////// search movie https://developers.themoviedb.org/3/search/search-movies
 const movieSearchByName = async (query, page) => {
-  try {
-    const objResult = [];
-    return await axios
-      .get(
-        `/search/movie?api_key=${KEY}&language=en-US&query=${query}&page=${page}`
-      )
-      .then(res => {
-        res.data.results.forEach(element => {
-          objResult.push(element);
-        });
-        console.log(
-          objResult.reduce(function (newArr, movie) {
-            if (movie.backdrop_path != null) {
-              newArr.push(movie);
-            }
-            return newArr;
-          }, [])
-        );
-        return objResult.reduce(function (newArr, movie) {
-          if (movie.backdrop_path != null) {
-            newArr.push(movie);
-          }
-          return newArr;
-        }, []);
-      });
-  } catch (error) {
-    console.error(error);
-  }
-
-  // const { data } = await axios.get(
-  //   `/search/movie?api_key=${KEY}&language=en-US&query=${query}&page=${page}`
-  // );
-  // console.log(data);
-  // return data;
+  const { data } = await axios.get(
+    `/search/movie?api_key=${KEY}&language=en-US&query=${query}&page=${page}`
+  );
+  console.log(data);
+  return data;
 };
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+///////////////////////////////////////////////////////////////// get details https://developers.themoviedb.org/3/movies/get-movie-details
 const getMovieDetails = async id => {
   const { data } = await axios.get(`/movie/${id}?api_key=${KEY}`);
   console.log(data);
   return data;
 };
 
+//////////////////////////////////////////////////////////////// get genres https://developers.themoviedb.org/3/genres/get-movie-list
 const getMovieGenres = async () => {
   const { data } = await axios.get(`/genre/movie/list?api_key=${KEY}`);
   console.log(data);
