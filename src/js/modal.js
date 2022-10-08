8bec2b4113ddd3a8f1d1c64e4770d0644590542b
Oliko136 <olchick136@gmail.com>
2022-10-09 00:46:02 +0400

    Modal opening added

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,4 +1,13 @@
-import renderModal from "./renderModal";
+import { renderModal, backdrop } from "./renderModal";
+
+const cardGallery = document.querySelector('.gallery');
+
+cardGallery.addEventListener('click', onModalOpen);
+
+function onModalOpen(e) {
+
+    backdrop.classList.remove('is-hidden');
+}
 
 fetch('https://api.themoviedb.org/3/movie/400?api_key=a5a9302ae32ac6fa75bc0508e4c74c0b')
     .then(response => response.json())
