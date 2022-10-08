985e1287025766161a6a5ec5f369f1538f893541
Oliko136 <olchick136@gmail.com>
2022-10-08 21:00:55 +0400

    Modal basic render and styling

---
+++ src/js/modal.js
@@ -0,0 +1,9 @@
+import renderModal from "./renderModal";
+
+fetch('https://api.themoviedb.org/3/movie/400?api_key=a5a9302ae32ac6fa75bc0508e4c74c0b')
+    .then(response => response.json())
+    .then(data => renderModal(data));
+
+
+
+
---
+++ src/js/renderModal.js
@@ -0,0 +1,27 @@
+const backdrop = document.querySelector('.backdrop');
+
+export default function renderModal({ poster_path, title, vote_average, vote_count, popularity, original_title, overview }) {
+    const markup = `<div class="modal">
+                        <button type="button" class="modal__btn-close" data-modal-close>
+                            <svg class="modal__icon-close" width="14" height="14">
+                            <use></use>
+                            </svg>
+                        </button>
+
+                        <img class="modal__image" src="https://image.tmdb.org/t/p/original/${poster_path}" alt="${title} poster">
+
+                        <h2 class="modal__title">${title}</h2>
+
+                        <p class="modal__info">Vote / Votes <span class="modal__info-value-vote">${vote_average}</span> / <span class="modal__info-value">${vote_count}</span></p>
+                        <p class="modal__info">Popularity <span class="modal__info-value">${popularity}</span></p>
+                        <p class="modal__info">Original Title <span class="modal__info-value">${original_title}</span></p>
+                        <p class="modal__info">Genre</p>
+
+                        <h3 class="modal__about">About</h3>
+                        <p class="modal__about-text">${overview}</p>
+
+                        <button type="button" class="modal__btn modal__btn-watched">Add to watched</button>
+                        <button type="button" class="modal__btn modal__btn-queue">Add to queue</button>
+                    </div>`;
+    backdrop.innerHTML = markup;
+}
