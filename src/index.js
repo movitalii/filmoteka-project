6e367981f1b2b328e021f104c1505ffc5ee0af4c
anfernee84 <eduard.rogulin@outlook.com>
2022-10-09 14:02:16 +0300

    added_storage.api_&&_added_call_function_at_API_query

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import './js/service_api';
 import './js/search-bar';
+import './js/storage_api';
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,17 +1,19 @@
 import ApiService from './api-service';
 import onCard from './card';
-
+// Ed import//
+import { saveInfo, getInfo, removeInfo } from './storage_api';
+/////////////
 const apiService = new ApiService();
 
 apiService.fetchImage().then(data => {
+  saveInfo(data.page, data.results); // добавил сохранение в локалсторедж при обращении к АПИ////////
   addArticleImage(data);
 });
 
 function addArticleImage(data) {
-  console.log('data')
+  console.log('data');
   console.log(data.results);
-  const cart = data.results.map(result => onCard(result)
-   ).join('');
-    // console.log('cart', cart)
+  const cart = data.results.map(result => onCard(result)).join('');
+  // console.log('cart', cart)
   document.querySelector(`.gallery`).insertAdjacentHTML('beforeend', cart);
 }
