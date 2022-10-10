30fcec0285dc403b85051877c27c9cb0a8f6e6b9
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-10-10 15:43:18 +0300

    Update card.js

--- src/js/card.js
+++ src/js/card.js
@@ -3,9 +3,9 @@ export default function onCard({poster_path, name, title, genre_ids, release_dat
     return `<div class="card">
     <img src="${BASE_IMAGE}${poster_path}" alt="A FISTFUL OF LEAD" class="card_image" width="300">
     <h2 class="card_name">${name||title}</h2>
-    <div class='card_text'>
-      <p class="card_title">${genre_ids }</p>
-      <p class='card_year'> | ${
+    <div class="card_text">
+      <p class="card_title">${genre_ids}</p>
+      <p class="card_year"> | ${
         release_date ? release_date.slice(0, 4) : first_air_date.slice(0, 4)
       }</p>      
     </div>
