8fb227a36f6dd079b64818b4db2d046453d693f9
Serhii Lazarenok <serhiilazar90@gmail.com>
2022-10-11 00:02:13 +0300

    modalCard

--- src/js/modal.js
+++ src/js/modal.js
@@ -9,6 +9,6 @@ function onModalOpen(e) {
     backdrop.classList.remove('is-hidden');
 }
 
-fetch('https://api.themoviedb.org/3/movie/400?api_key=a5a9302ae32ac6fa75bc0508e4c74c0b')
-    .then(response => response.json())
-    .then(data => renderModal(data));
+// fetch('https://api.themoviedb.org/3/movie/400?api_key=a5a9302ae32ac6fa75bc0508e4c74c0b')
+//     .then(response => response.json())
+//     .then(data => renderModal(data));
