5bc2a1be3ecbf1e87c2ba8ec5fd97ca14cc72992
Albertukraine <101990726+Albertukraine@users.noreply.github.com>
2022-10-11 11:57:03 +0300

    try to merge

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 
 import './sass/index.scss';
-import './js/render-cards-library';
 import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
 import './js/team-modal';
 import './js/service_api';
