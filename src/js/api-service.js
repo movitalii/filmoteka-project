75ee018b5b047f90541b26d978ac44433f022e2c
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-10-11 14:59:08 +0300

    add spinner

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,76 +1,86 @@
-import axios from "axios";
+import axios from 'axios';
 
 const API_KEY = '3ff086ca8fded08ba42938358b3327b4';
 const BASE_URL = `https://api.themoviedb.org/3/`;
+const spinner = document.getElementById('spinner');
 
 export default class ApiService {
-   #id
-    #searchQuery;
-    constructor (){
-       
-        this.page = 1;
-        this.#id = null;
-        this.#searchQuery = '';   
-        console.log('searchQuery', this.#searchQuery)
-        console.log('id', this.#id)
-     }   
+  #id;
+  #searchQuery;
+  constructor() {
+    this.page = 1;
+    this.#id = null;
+    this.#searchQuery = '';
+    console.log('searchQuery', this.#searchQuery);
+    console.log('id', this.#id);
+  }
 
-    async fetchGenres() {      
-       
-         const response  = await axios.get(`${BASE_URL}genre/movie/list?api_key=${API_KEY}&language=en-US`)
-          const {genres} = response.data;          
-          
-            this.page += 1;        
-           
-            return {genres};
-         };    
+  async fetchGenres() {
+    const response = await axios.get(
+      `${BASE_URL}genre/movie/list?api_key=${API_KEY}&language=en-US`
+    );
+    const { genres } = response.data;
 
-     async fetchImage() {       
-        
-         const response  = await axios.get(`${BASE_URL}trending/all/day?api_key=${API_KEY}`)
-          const data = response.data;          
-         
-            this.page += 1;        
+    this.page += 1;
+
+    return { genres };
+  }
+
+  async fetchImage() {
+    spinner.removeAttribute('hidden');
+    const response = await axios.get(
+      `${BASE_URL}trending/all/day?api_key=${API_KEY}`
+    );
+    spinner.setAttribute('hidden', '');
+    const data = response.data;
+
+    this.page += 1;
     //  console.log('data', data.results)
-            return data;
-         };        
-         
-         async fetchAllFilms() {                  
-            
-             const response  = await axios.get(`${BASE_URL}movie/${this.#id}?api_key=${API_KEY}&language=en-US`)
-            const allFilm = response.data;          
-             
-                this.page += 1;        
-         console.log('all', allFilm)
-                return allFilm;
-             }; 
+    return data;
+  }
 
-             async fetchFundFilms() {               
-              
-                 const response  = await axios.get(`${BASE_URL}search/movie?api_key=${API_KEY}&query=${this.#searchQuery}`)
-                const FundFilm = response.data;          
-                 
-                    this.page += 1; 
+  async fetchAllFilms() {
+    spinner.removeAttribute('hidden');
+    const response = await axios.get(
+      `${BASE_URL}movie/${this.#id}?api_key=${API_KEY}&language=en-US`
+    );
+    spinner.setAttribute('hidden', '');
+    const allFilm = response.data;
 
-             console.log('fund', FundFilm)
-                    return FundFilm;
-                 }; 
-   
-     clearForm() {
-        this.page = 1;
-    }
+    this.page += 1;
+    console.log('all', allFilm);
+    return allFilm;
+  }
 
-    get query() {
-        return this.searchQuery;
-    }
-    set query(newQuery){
-        this.#searchQuery = newQuery;
-    }
-   
-    get id() {
-        return this.id;
-    }
-    set id(newId){
-        this.#id= newId;
-    }
-};
+  async fetchFundFilms() {
+    spinner.removeAttribute('hidden');
+    const response = await axios.get(
+      `${BASE_URL}search/movie?api_key=${API_KEY}&query=${this.#searchQuery}`
+    );
+    spinner.setAttribute('hidden', '');
+    const FundFilm = response.data;
+
+    this.page += 1;
+
+    console.log('fund', FundFilm);
+    return FundFilm;
+  }
+
+  clearForm() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.#searchQuery = newQuery;
+  }
+
+  get id() {
+    return this.id;
+  }
+  set id(newId) {
+    this.#id = newId;
+  }
+}
