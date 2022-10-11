97b07ca38034b459a94b92a1f8ebe4679dd6d507
Aleksandr Cheverda <alexar@ua.fm>
2022-10-11 23:41:27 +0300

    upd files
    update
    api-service.js
    search-bar.js
    header-home.html

--- src/js/search-bar.js
+++ src/js/search-bar.js
@@ -1,59 +1,29 @@
-// import { movieSearchByName } from './service_api';
-
 import ApiService from './api-service';
-import renderFilmsMarkup from './templates/renderFilmsMarkup';
 
-const searchFormRef = document.querySelector('.form-group');
-const errorMessage = document.querySelector('.error-notification');
-const input = document.querySelector(`.form-group__input`)
-// const filmsApi = new movieSearchByName();
-// const filmsApi = new API_service();
-
-
-
+const refs = {
+  searchForm: document.querySelector('#form-search'),
+  input: document.querySelector('.form-group__input'),
+  // errorMessage: document.querySelector('.error-notification'),
+};
 const apiService = new ApiService();
 
-// function onFundName() {
-//   apiService.query = 'dog';
-//   apiService.fetchFundFilms();
-// }
-// onFundName()
+console.log(apiService);
 
+refs.searchForm.addEventListener('submit', onFormSubmit);
+// refs.input.addEventListener('input', onFundName);
 
-searchFormRef.addEventListener('submit', onFormSubmit);
-input.addEventListener(`input`, onFundName)
-
-console.log(searchFormRef);
-
-function onFundName(e) {
-    apiService.query = e.currentTarget.value;
-    console.log(e.currentTarget.value)
-    apiService.fetchFundFilms();
-  }
- 
-
-function onFormSubmit(evt) {
-  evt.preventDefault();
-  // console.log(evn)
-  // apiService.query = evt.currentTarget.value;
-
-  // apiService.fetchFundFilms();
-
-  // try {
-  //   apiService.searchQuery = evt.currentTarget.elements.searchQuery.value.trim();
-  //   if (filmsApi.searchQuery === '') return;
-
-  //   const films = await filmsApi.fetchMoviesByKeyword();
-  //   if (films.length === 0) {
-  //     addErrorStyles();
-  //     errorMessage.style.display = 'block';
-  //   } else {
-  //     resetErrorStyles();
-  //   }
-  //   renderFilmsMarkup(films);
-
-  //   searchFormRef.reset();
-  // } catch (error) {
-  //   console.log(error);
-  // }
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log('click');
+  apiService.query = e.currentTarget.elements.searchQuery.value;
+  apiService.fetchFundFilms();
+  apiService.fetchFundFilms().then(data => {
+    moves = data;
+  });
 }
+
+// function onFundName(e) {
+//     apiService.query = e.currentTarget.value.trim();
+//     console.log(e.currentTarget.value)
+//     apiService.fetchFundFilms();
+// }
