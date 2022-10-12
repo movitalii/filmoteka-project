e7f3aca9790ec2637073bfd764576d95acbd764b
anfernee84 <eduard.rogulin@outlook.com>
2022-10-12 10:49:05 +0300

    add_chages_from_index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import './sass/index.scss';
-import './js/fetchImages';
-import './js/modal';
 import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
+import './js/render-cards-library';
+import './js/api-service';
 import './js/team-modal';
 import './js/service_api';
 import './js/apiSevice';
 import './js/search-bar';
 import './js/storage_api';
-
+import './js/fetchImages';
+import './js/modal';
+import './js/modalCard';
+import './js/scroll-up-btn';
