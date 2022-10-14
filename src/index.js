a82a41d1990be1e2a5d54272d3da1afa6af2574b
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-10-14 14:30:34 +0300

    add modal styles

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import './js/team-modal';
 import './js/search-bar';
 import './js/storage_api';
 import './js/fetchImages';
-import './js/modal';
 import './js/modalCard';
 import './js/scroll-up-btn';
 import './js/pagination';
