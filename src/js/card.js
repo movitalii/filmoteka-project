091d4d77cdaa16d4ba9e592a82e86a3a601e0d3c
Albertukraine <101990726+Albertukraine@users.noreply.github.com>
2022-10-14 12:52:05 +0300

    add loading lazy to cards but not in modal

--- src/js/card.js
+++ src/js/card.js
@@ -1,7 +1,8 @@
 export default function onCard({poster_path, name, title, genre_ids, release_date, vote_average, first_air_date}) {
     const BASE_IMAGE = `https://image.tmdb.org/t/p/original`
     return `<div class="card">
-    <img src="${BASE_IMAGE}${poster_path}" alt="A FISTFUL OF LEAD" class="card_image" width="300">
+    <img src="${BASE_IMAGE}${poster_path}" loading="lazy" alt="A FISTFUL OF LEAD" class="card_image" width="300"
+    >
     <h2 class="card_name">${name||title}</h2>
     <div class="card_text">
       <p class="card_title">${genre_ids}</p>
