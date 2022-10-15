6d31db40e1bb7f23b1b0f88df5fe89b56ada31fd
andrii-pozniak <andriipozniak@gmail.com>
2022-10-15 18:24:32 +0300

    add fix card

--- src/js/card.js
+++ src/js/card.js
@@ -1,5 +1,6 @@
 export default function onCard({poster_path, name, title, genre_ids, release_date, vote_average, first_air_date}) {
-    const BASE_IMAGE = `https://image.tmdb.org/t/p/original`
+  
+  const BASE_IMAGE = `https://image.tmdb.org/t/p/original`
     return `<div class="card">
     <img src="${BASE_IMAGE}${poster_path}" loading="lazy" alt="A FISTFUL OF LEAD" class="card_image" width="300"
     >
@@ -7,7 +8,7 @@ export default function onCard({poster_path, name, title, genre_ids, release_dat
     <div class="card_text">
       <p class="card_title">${genre_ids}</p>
       <p class="card_year"> | ${
-        release_date ? release_date.slice(0, 4) : first_air_date.slice(0, 4)
+        release_date ? release_date.slice(0, 4) : first_air_date
       }</p>      
     </div>
 </div>`    
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -28,10 +28,17 @@ console.log(genres)
 export default function addArticleImage(data) {
   
   // console.log('image', data);
-  const cart = data.results.map(result => {
+  const cart = data.results.flatMap(result => {
     let genresArr = [];
-    result.genre_ids.forEach(genreID => {
-      // console.log(genreID)
+    if(result.gender === 0 || result.gender === 1 || result.gender === 2 ){
+      result.genre_ids = [' Drama'];
+      result.first_air_date = "2019-10-17";
+     return result.genre_ids;
+    }
+   
+    
+    result.genre_ids.forEach((genreID) => {
+     
       genres.forEach(genOBJ => {
         // console.log(genOBJ)
         if (genreID === genOBJ.id) {
@@ -51,10 +58,24 @@ export default function addArticleImage(data) {
       genresArr.push('No genres');
     }
     result.genre_ids = genresArr;
+    if(result.gender){
+      
+
+    }
     // console.log(result)
     // console.log('odject', Object.values(result.genre_ids)) 
+    if(result.first_air_date === '') {
+      result.first_air_date = `2021`
+    }
+    if(result.backdrop_path == null){
+      result.backdrop_path = "/xHLwNxk7Kgpatru7advnbnBGtgf.jpg"
+      return result.backdrop_path;
+    }
+    result.release_date === '' ? result.release_date = `2015`
+          : result.release_date;
     
-    
+    // console.log(result.poster_path)
+        
     return result;
   }).map(result => onCard(result)).join('');
   // console.log('cart', cart)
